4801af90a1ac4399bb1603b91e63ea52a7a812b8
mdang8 <mdang808@gmail.com>
2017-03-02 17:49:44 -0500

    Changed some things in index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
+const verify_token = process.env.FB_WEBHOOK_VERIFY_TOKEN;
+const page_access_token = process.env.FB_PAGE_ACCESS_TOKEN;
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(bodyParser.urlencoded({
@@ -18,14 +21,29 @@ app.get('/', function (req, res) {
 });
 
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'dang_rhee_verify_token') {
+    if (req.query['hub.verify_token'] === verify_token) {
         res.send(req.query['hub.challenge']);
     }
 
-    res.send('Error: wrong token');
+    res.send('Wrong token.');
 });
 
 app.listen(app.get('port'), function() {
-    console.log('Running on port', app.get('port'))
+    console.log('Running on port', app.get('port'));
 });
 
+app.post('/webhook/', function (req, res) {
+    let messagingEvents = req.body.entry[0].messaging;
+
+    for (let i = 0; i < messagingEvents.length; i++) {
+        let event = req.body.entry[0].messaging[i];
+        let sender = event.sender.id;
+
+        if (event.message && event.message.text) {
+            let messageText = event.message.text;
+
+        }
+    }
+
+    res.sendStatus(200);
+});
