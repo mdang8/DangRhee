367d5efc69d14faba3f651e06ed9714962aa974c
mdang8 <mdang808@gmail.com>
2017-04-11 16:51:29 -0400

    testing

--- index.js
+++ index.js
@@ -36,21 +36,34 @@ app.listen(app.get('port'), function() {
     console.log('Running on port', app.get('port'));
 });
 
-app.post('/webhook/', function (req, res) {
-    let messagingEvents = req.body.entry[0].messaging;
+// app.post('/webhook/', function (req, res) {
+//     let messagingEvents = req.body.entry[0].messaging;
+//
+//     for (let i = 0; i < messagingEvents.length; i++) {
+//         let event = req.body.entry[0].messaging[i];
+//         let sender = event.sender.id;
+//
+//         if (event.message && event.message.text) {
+//             let messageText = event.message.text;
+//
+//             sendReply(sender, messageText);
+//         }
+//     }
+//
+//     res.sendStatus(200);
+// });
 
-    for (let i = 0; i < messagingEvents.length; i++) {
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging;
+    for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i];
         let sender = event.sender.id;
-
         if (event.message && event.message.text) {
-            let messageText = event.message.text;
-
-            sendReply(sender, messageText);
+            let text = event.message.text;
+            sendReply(sender, "Text received, echo: " + text.substring(0, 200));
         }
     }
-
-    res.sendStatus(200);
+    res.sendStatus(200)
 });
 
 app.post('/privacy-policy/', function (req, res) {
